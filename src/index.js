4b591fe2cbe73c3d11da10b38f14783aa024f09a
roshanpaturkar <paturkarr@gmail.com>
2021-02-01 11:38:21 +0530

    run script configured and port number is loaded from env var

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { request } = require('express')
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (request, response) => {
     response.send('Welcome to the DSC GHRCE Certificate Manager!')
