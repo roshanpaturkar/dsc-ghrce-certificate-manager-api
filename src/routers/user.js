29f449f16ee3f876bffdbab798779361117f20fe
roshanpaturkar <paturkarr@gmail.com>
2021-02-01 17:07:44 +0530

    upload avatar bug fixed

--- src/routers/user.js
+++ src/routers/user.js
@@ -102,8 +102,12 @@ const upload = multer ({
     })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (request, response) => {
-    const buffer = await sharp(request.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
-    request.user.avatar = buffer
+    try {
+        const buffer = await sharp(request.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+        request.user.avatar = buffer
+    } catch (error) {
+        response.status(400).send({ error: 'Image data not found!'})
+    }
     await request.user.save()
     response.send()
 }, (error, request, response, next) => {
