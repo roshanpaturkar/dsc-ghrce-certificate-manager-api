6f5389f1005ebe59bd99354102015c53cc1d3491
roshanpaturkar <paturkarr@gmail.com>
2021-02-27 15:12:00 +0530

    API Key is now hide in headers

--- src/middleware/apiKey.js
+++ src/middleware/apiKey.js
@@ -1,20 +1,29 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const apiKey = async (request, response, next) => {
+  try {
+    const key = request.header("API-Key");
+
+    if (!key) {
+        throw new Error();
+    }
 
     var config = {
-        method: 'get',
-        url: `${process.env.API_KEY}${request.params.key}`
+      method: "get",
+      url: `${process.env.API_KEY}${key}`,
     };
 
     axios(config)
-    .then(function (response) {
-        request.key = response.data
-        next()
-    })
-    .catch(function (error) {
-        return response.status(error.response.status).send(error.response.data)
-    });
-}
+      .then(function (response) {
+        request.key = response.data;
+        next();
+      })
+      .catch(function (error) {
+        return response.status(error.response.status).send(error.response.data);
+      });
+  } catch (error) {
+    response.status(401).send({ error: "API Key is missing!" });
+  }
+};
 
-module.exports = apiKey
+module.exports = apiKey;
