979e4f835b05e96108c58833591a967dc7fb8802
roshanpaturkar <paturkarr@gmail.com>
2021-02-28 18:44:20 +0530

    security test 05

--- src/routers/user.js
+++ src/routers/user.js
@@ -162,6 +162,7 @@ router.get('/users/me/avatar', apiKey, auth, async (request, response) => {
 
 router.get('/users/:id/avatar', async (request, response) => {
     console.log(request.host === process.env.SAFEHOST);
+    console.log(request);
     try {
         const user = await User.findById(request.params.id)
 
