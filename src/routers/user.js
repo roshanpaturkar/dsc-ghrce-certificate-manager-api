d9fa67fca2f505648a35ef4805a77a6e6dba6651
roshanpaturkar <paturkarr@gmail.com>
2021-02-28 17:47:19 +0530

    api key security layer removed from /users/-userid-/avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -160,7 +160,7 @@ router.get('/users/me/avatar', apiKey, auth, async (request, response) => {
     }
 })
 
-router.get('/users/:id/avatar', apiKey, async (request, response) => {
+router.get('/users/:id/avatar', async (request, response) => {
     try {
         const user = await User.findById(request.params.id)
 
