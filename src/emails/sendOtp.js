5690437fdb778a74ce7d8b37beda18e8837eb059
roshanpaturkar <paturkarr@gmail.com>
2021-03-22 19:56:12 +0530

    code fixed

--- src/emails/sendOtp.js
+++ src/emails/sendOtp.js
@@ -2,6 +2,7 @@ const transporter = require('./account')
 
 const sendOtp = async (name, otpData) => {
     const body = `<b>Hello ${name}</b><br><br>Your OTP is <b>${otpData.otp}</b>. <i>Be quick, it will be expired on ${otpData.expiryDateTime.toLocaleString()}</i>.<br>Thank You! <br><br><b>Best</b> <br><i>Server Admin</i>`
+    
     await transporter.sendMail({
         from: '"DSC GHRCE 📪" certificate.dscghrce@gmail.com',
         to: otpData.email,
