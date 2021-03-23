e952a389b7d99ddd72f1b9287e3acfc682fa3d55
roshanpaturkar <paturkarr@gmail.com>
2021-03-23 11:28:05 +0530

    le date ka jugad

--- src/utility/getDate.js
+++ src/utility/getDate.js
@@ -9,6 +9,11 @@ const certificateIssueDate = function () {
       "Friday",
       "Saturday"
     ),
+    leDateEnder = new Array(
+      '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th',
+      '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th',
+      '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st'
+    )
     dayOfWeek = weekday[objToday.getDay()],
     domEnder = (function () {
       var a = objToday;
@@ -52,7 +57,7 @@ const certificateIssueDate = function () {
         : objToday.getSeconds(),
     curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
 
-  var today = dayOfMonth + " " + curMonth + ", " + curYear + " (INDIA)";
+  var today = leDateEnder[objToday.getDate()-1] + " " + curMonth + ", " + curYear + " (INDIA)";
   return today;
 };
 
