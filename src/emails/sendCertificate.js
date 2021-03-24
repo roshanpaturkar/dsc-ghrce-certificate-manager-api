90d00fa84195857f06b984bcdde56d1937734511
roshanpaturkar <paturkarr@gmail.com>
2021-03-24 21:29:20 +0530

    mailer test 17

--- src/emails/sendCertificate.js
+++ src/emails/sendCertificate.js
@@ -1,5 +1,4 @@
 const transporter = require('./account')
-import { SMTPClient } from 'emailjs';
 
 const sendCertificate = async (certificateData, eventName) => {
     const body = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
