98d6c35c8207b53f8c99aaba59e96a1265e95e4c
roshanpaturkar <paturkarr@gmail.com>
2021-04-01 15:38:18 +0530

    send individual email to user route implemented

--- src/routers/certificate.js
+++ src/routers/certificate.js
@@ -4,6 +4,7 @@ const apiKey = require('../middleware/apiKey')
 
 const Event = require('../models/event')
 const Certificate = require('../models/certificate')
+const sendCertificate = require('../emails/sendCertificate')
 
 const router = new express.Router()
 
@@ -13,6 +14,18 @@ router.get('/certificates/:id', apiKey, async (request, response) => {
         const event = await Event.findOne({ eventID: certificate.eventID })
         
         response.send({ event, certificate })
+    } catch (error) {
+        response.status(404).send({ error: 'Invalid Certificate ID!'})
+    }
+})
+
+router.get('/sendCertificate/:id', apiKey, async (request, response) => {
+    try {
+        const certificate = await Certificate.findOne({ certificateID: request.params.id })
+        const event = await Event.findOne({ eventID: certificate.eventID })
+        sendCertificate(certificate, event.eventName)
+
+        response.send({message: `Certificate sent to ${certificate.name} on ${certificate.email}`})
     } catch (error) {
         response.status(404).send({ error: 'Invalid Certificate ID!'})
     }
