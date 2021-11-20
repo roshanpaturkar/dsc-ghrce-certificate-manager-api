1c5333a7983ba62883a98a4b6adc685343c6b637
Roshan Paturkar <paturkarr@gmail.com>
2021-11-21 01:17:48 +0530

    code fixed

--- src/routers/certificate.js
+++ src/routers/certificate.js
@@ -14,7 +14,7 @@ const admin = require('../middleware/admin')
 const router = new express.Router()
 
 router.get('/certificates/:id', apiKey, async (request, response) => {
-    legacyEventIds = ['82cf194df831b2979522409a048ae0', 'c89414443fc7a42aca659b2fcf8ada', 'cf53a49eb74d25a981dcf7c5b17cd5', '57b7583ac04e94853283477ee1ca4c']
+    legacyEventIds = ['82cf194df831b2979522409a048ae0', 'c89414443fc7a42aca659b2fcf8ada', 'cf53a49eb74d25a981dcf7c5b17cd5', '57b7583ac04e94853283477ee1ca4c', '36072924de276a1d6d4cbe']
     try {
         const certificate = await Certificate.findOne({ certificateID: request.params.id })
         const event = await Event.findOne({ eventID: certificate.eventID })
