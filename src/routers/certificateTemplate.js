17eefac0209e7436061c47dc57e7def73b693f8d
Roshan Paturkar <paturkarr@gmail.com>
2021-11-25 20:31:24 +0530

    code test 02

--- src/routers/certificateTemplate.js
+++ src/routers/certificateTemplate.js
@@ -155,7 +155,8 @@ router.delete('/certificate/template/:id', apiKey, auth, admin, async (request, 
         const certificateTemplateImageCheck = await CertificateTemplate.findOne({certificateTemplateImageId: templateImageId})
         if (!certificateTemplateImageCheck) {
             console.log(certificateTemplate.certificateTemplateImageId);
-            const data = await CertificateTemplateImage.findOneAndDelete(templateImageId)
+            const image = await CertificateTemplateImage.find(templateImageId)
+            const data = await image.remove()
             console.log(data);
         }
         response.send({message: 'Certificate Template deleted successfully!'})
