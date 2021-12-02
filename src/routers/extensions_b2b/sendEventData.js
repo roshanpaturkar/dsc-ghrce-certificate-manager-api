7853efd5dd3c6d9f7b5e7501871ece3237b38e86
Roshan Paturkar <paturkarr@gmail.com>
2021-12-02 15:58:54 +0530

    publish by userId valorized by B2B user userId

--- src/routers/extensions_b2b/sendEventData.js
+++ src/routers/extensions_b2b/sendEventData.js
@@ -5,9 +5,7 @@ const apiKey = require('../../middleware/apiKey')
 const router = new express.Router()
 
 router.post('/b2b/certificate/manager/sendEventData', apiKey, async (request, response) => {
-    userData = {
-        
-    }
+    request.body.publishedBy.userID = '61a8e841d9a7c200161be3d2'
     console.log(request.body);
     response.send('Welcome to the DSC GHRCE Certificate Manager!')
 })
