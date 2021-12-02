3917cfb09c7782a7be855037ea037ede2a32351f
Roshan Paturkar <paturkarr@gmail.com>
2021-12-02 21:57:21 +0530

    event data valorized, deploying for test

--- src/routers/extensions_b2b/sendEventData.js
+++ src/routers/extensions_b2b/sendEventData.js
@@ -1,12 +1,34 @@
 const express = require('express')
+const cryptoRandomString = require('crypto-random-string');
+
+const Pool = require('../../models/pool');
 
 const apiKey = require('../../middleware/apiKey')
 
 const router = new express.Router()
 
 router.post('/b2b/certificate/manager/sendEventData', apiKey, async (request, response) => {
-    request.body.publishedBy.userID = '61a8e841d9a7c200161be3d2'
-    console.log(request.body);
+    try {
+        request.body.publishedBy.userID = '61a8e841d9a7c200161be3d2'
+
+        certificates = []
+        request.body.attendeesList.forEach(attendee => {
+            certificates.push({
+                eventID: request.body.eventID,
+                certificateID: cryptoRandomString({length: 20}),
+                name: attendee.name,
+                email: attendee.email,
+            })
+        })
+        request.body.certificates = certificates
+
+        const pool = new Pool(request.body)
+        await pool.save()
+    }  catch (error) {
+        console.log(error)
+        response.status(500).send(error)
+    }
+
     response.send('Welcome to the DSC GHRCE Certificate Manager!')
 })
 
